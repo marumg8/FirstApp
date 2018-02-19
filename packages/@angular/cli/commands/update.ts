60c6a7a6fbc7d08d67bcd8819f9fee63c6e69f3b
marumg8 <maru.mg8@gmail.com>
2018-02-18 19:32:19 -0600

    cargar start

---
+++ packages/@angular/cli/commands/update.ts
@@ -0,0 +1,42 @@
+const Command = require('../ember-cli/lib/models/command');
+import { UpdateTask } from '../tasks/update';
+
+export interface UpdateOptions {
+  schematic?: boolean;
+}
+
+const UpdateCommand = Command.extend({
+  name: 'update',
+  description: 'Updates your application.',
+  works: 'everywhere',
+  availableOptions: [
+    {
+      name: 'dry-run',
+      type: Boolean,
+      default: false,
+      aliases: ['d'],
+      description: 'Run through without making any changes.'
+    },
+    {
+      name: 'next',
+      type: Boolean,
+      default: false,
+      description: 'Install the next version, instead of the latest.'
+    }
+  ],
+
+  anonymousOptions: [],
+
+  run: function(commandOptions: any) {
+    const schematic = '@schematics/package-update:all';
+
+    const updateTask = new UpdateTask({
+      ui: this.ui,
+      project: this.project
+    });
+
+    return updateTask.run(schematic, commandOptions);
+  }
+});
+
+export default UpdateCommand;
